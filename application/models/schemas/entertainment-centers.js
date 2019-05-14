cb8aec91ed63f131cc52c5afa1da73e21d27d3f9
Miguel <migueljimenezaquino@gmail.com>
2019-05-13 20:52:57 -0400

    add colletion

--- application/models/schemas/entertainment-centers.js
+++ application/models/schemas/entertainment-centers.js
@@ -5,11 +5,20 @@ privacy: {type: Schema.Types.ObjectId, ref: 'Privacies', required: false},
 user_published: {type: Schema.Types.ObjectId, ref: 'Users'},
 name:{ type: String, required: false},
 address:{ type: String, required: false},
+latitud:{ type: String, required: false},
+longitud:{ type: String, required: false},
 qualification:{ type: Number, max:5, min:0, default: 0},
 images:[{
   image: {type: String, required: false},
   primary: {type: Boolean, required: false, default: false}
  }],
+
+ schedules:[{
+   day: {type: Schema.Types.ObjectId, ref: 'EntertainmentCentersDay'},
+   hour_open: {type: Boolean, required: false, default: false},
+   hour_close: {type: Boolean, required: false, default: false}
+  }],
+
 date_create: {
     type: Date,
     required: true,
